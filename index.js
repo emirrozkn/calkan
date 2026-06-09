fd31415e2eb5576d0a2dd42aac3bbc3c1daa1c26
emirrozkn <emir.ozkn2006@gmail.com>
2026-06-09 01:06:17 -0700

    feat: v0.1 bot connects and goes online

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+client.on('clientReady', () => {
+    console.log(`✅ ${client.user.tag} is online!`);
+});
+
+client.login(process.env.BOT_TOKEN);
